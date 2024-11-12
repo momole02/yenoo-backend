3e282d126a41bea7bcd4e8fdb843fdae310249dd
Marc AYENON <marc.ayenon@inexa-ci.com>
2024-11-12 08:48:37 +0000

    feat(onboarding): added some logging

--- src/api/onboarding/controllers/onboarding.ts
+++ src/api/onboarding/controllers/onboarding.ts
@@ -4,10 +4,14 @@
 
 import Joi from "joi";
 import bcrypt from "bcrypt"
+import logging from "logging"
 
+const logger = logging("Onboarding API")
 
 export default {
+
   signup: async (ctx, next) => {
+
     const schema = Joi.object({
       firstName: Joi.string().required(),
       lastName: Joi.string().required(),
@@ -21,12 +25,12 @@ export default {
       ctx.request.body
     )
     if (result.error) {
-      ctx.throw(400, "Bad request", result.error)
+      logger.error("signup() : validation error", result.error)
+      ctx.throw(400, "Bad request", { message: result.error.message })
       return;
     }
 
     const data = result.value
-
     const user = strapi.documents("plugin::users-permissions.user").create({
       data: {
         username: data.email,
@@ -43,6 +47,8 @@ export default {
       }
     })
 
+    logger.info(`created user: ${data.lastName} ${data.firstName}`)
+
     ctx.status = 201
     ctx.body = {
       user,
@@ -58,7 +64,10 @@ export default {
 
     const result = schema.validate(ctx.request.body)
     if (result.error) {
-      ctx.throw(400, "Validation error", result.error)
+      logger.error("auth(): validation error", result.error)
+      ctx.throw(400, "Validation error", {
+        message: result.error.message
+      })
       return;
     }
 
@@ -74,16 +83,21 @@ export default {
       }
     )
     if (!searchResult.length) {
-      ctx.throw(401, "Invalid credentials", { data })
+      logger.error(`auth(): user not found ${data.identifier}`)
+      ctx.throw(401, "Invalid credentials",)
       return;
     }
 
     const user = searchResult[0]
     if (!await bcrypt.compare(data.password, user.password)) {
-      ctx.throw(401, "Invalid credentials", { data })
+      logger.error(`auth(): invalid password for ${user.firstName} ${user.lastName}`, {
+        data
+      })
+      ctx.throw(401, "Invalid credentials")
       return
     }
     if (!user.is2FAEnabled) {
+      logger.warn(`2FA auth disabled for user ${user.firstName} ${user.lastName} `)
       const jwt = strapi.plugins["users-permissions"]
         .services
         .jwt
@@ -96,6 +110,9 @@ export default {
       }
     } else {
       const twoFactorAuthCode = `${Math.random() * 90000 + 10000}`;
+      logger.info(`going for 2FA auth for ${user.firstName} ${user.lastName}`, {
+        twoFactorAuthCode
+      })
       strapi.documents("plugin::users-permissions.user").update({
         documentId: user.documentId,
         data: {
